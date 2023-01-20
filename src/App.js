fa50f195788f24113b5bcc699e56b78227d221d8
Zac Davis <thatzacdavis@gmail.com>
2023-01-19 21:02:01 -0500

    Add initial rectangle drawing, moving, resizing functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, useMemo } from 'react';
+import { Rect, Stage, Layer } from 'react-konva';
+import { Rectangle } from './components/Rectangle';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+const initialRectangles = [
+  {
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 100,
+    fill: 'red',
+    id: 'rect1',
+  },
+  {
+    x: 150,
+    y: 150,
+    width: 100,
+    height: 100,
+    fill: 'green',
+    id: 'rect2',
+  },
+];
+
+const App = () => {
+  const [annotations, setAnnotations] = useState([]);
+  const [newAnnotation, setNewAnnotation] = useState([]);
+  const annotationsToDraw = useMemo(() => [...annotations, ...newAnnotation], [annotations, newAnnotation]);
+
+  const [rectangles, setRectangles] = useState(initialRectangles);
+  const [selectedId, setSelectedId] = useState(null);
+  const [draggingId, setDraggingId] = useState(null)
+
+  const handleMouseDown = e => {
+    const clickedOnEmpty = e.target === e.target.getStage();
+
+    if (selectedId) {
+      if (clickedOnEmpty) {
+        setSelectedId(null);
+        return
+      }
+    }
+
+    if (!selectedId && !draggingId && clickedOnEmpty && newAnnotation.length === 0) {
+      const { x, y } = e.target.getStage().getPointerPosition();
+      setNewAnnotation([{ x, y, width: 0, height: 0, key: "0" }]);
+    }
+  };
+
+  const handleMouseUp = event => {
+    if (!selectedId && !draggingId && newAnnotation.length === 1) {
+      const sx = newAnnotation[0].x;
+      const sy = newAnnotation[0].y;
+      const { x, y } = event.target.getStage().getPointerPosition();
+      const annotationToAdd = {
+        x: sx,
+        y: sy,
+        width: x - sx,
+        height: y - sy,
+        key: annotations.length + 1
+      };
+      annotations.push(annotationToAdd);
+      setNewAnnotation([]);
+      setAnnotations(annotations);
+    }
+
+    if (draggingId) {
+      setDraggingId(null)
+    }
+  };
+
+  const handleMouseMove = event => {
+    if (!selectedId && !draggingId && newAnnotation.length === 1) {
+      const sx = newAnnotation[0].x;
+      const sy = newAnnotation[0].y;
+      const { x, y } = event.target.getStage().getPointerPosition();
+      setNewAnnotation([
+        {
+          x: sx,
+          y: sy,
+          width: x - sx,
+          height: y - sy,
+          key: "0"
+        }
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    if (annotations.length > 0) {
+      const rectToAdd = {
+        x: annotations[0].x,
+        y: annotations[0].y,
+        width: annotations[0].width,
+        height: annotations[0].height,
+        fill: 'salmon',
+        id: uuidv4(),
+      }
+
+      setRectangles([...rectangles, { ...rectToAdd }])
+      setAnnotations([])
+    }
+  }, [annotations, rectangles])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onTouchStart={handleMouseDown}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+    >
+      <Layer>
+        {rectangles.map((rect, i) => {
+          return (
+            <Rectangle
+              key={i}
+              shapeProps={rect}
+              isSelected={rect.id === selectedId}
+              onSelect={() => {
+                setSelectedId(rect.id);
+              }}
+              onChange={(newAttrs) => {
+                const rects = rectangles.slice();
+                rects[i] = newAttrs;
+                setRectangles(rects);
+              }}
+              onDragStart={() => setDraggingId(rect.id)}
+              // onDragEnd={() => setDraggingId(null)}
+            />
+          );
+        })}
+        {annotationsToDraw.map(value => {
+          return (
+            <Rect
+              x={value.x}
+              y={value.y}
+              width={value.width}
+              height={value.height}
+              fill="transparent"
+              stroke="black"
+            />
+          );
+        })}
+      </Layer>
+    </Stage>
   );
-}
+};
 
 export default App;
---
+++ src/components/Rectangle.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Rect, Transformer } from 'react-konva';
+
+export const Rectangle = ({ shapeProps, isSelected, onSelect, onChange, onDragStart, onDragEnd }) => {
+    const shapeRef = React.useRef();
+    const transformerRef = React.useRef();
+  
+    React.useEffect(() => {
+      if (isSelected) {
+        transformerRef.current.nodes([shapeRef.current]);
+        transformerRef.current.getLayer().batchDraw();
+      }
+    }, [isSelected]);
+  
+    return (
+      <>
+        <Rect
+          onClick={onSelect}
+          onTap={onSelect}
+          ref={shapeRef}
+          {...shapeProps}
+          draggable
+          onDragStart={(e) => {
+            onDragStart && onDragStart()
+          }}
+          onDragEnd={(e) => {
+            onChange({
+              ...shapeProps,
+              x: e.target.x(),
+              y: e.target.y(),
+            });
+
+            onDragEnd && onDragEnd()
+          }}
+          onTransformEnd={(e) => {
+            const node = shapeRef.current;
+            onChange({
+              ...shapeProps,
+              x: node.x(),
+              y: node.y(),
+            });
+          }}
+        />
+        {isSelected && (
+          <Transformer ref={transformerRef} />
+        )}
+      </>
+    );
+  };
