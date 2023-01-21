954df4233b4ee4457890e9b446477b704d748837
Zac Davis <thatzacdavis@gmail.com>
2023-01-20 19:39:54 -0500

    Add basic styling via Tailwind

--- src/components/EditSection.jsx
+++ src/components/EditSection.jsx
@@ -4,27 +4,31 @@
 export const EditSection = ({ selectedId, setSelectedId, rectangles, setRectangles, newColor, setNewColor }) => {
     return (
         <>
-        <button onClick={() => setRectangles([])}>Clear</button><div>
-            <h1>Selected Rectangle</h1>
-            {!selectedId
-                ? (<span>None</span>)
-                : (<div key={selectedId}>
-                    <label>ID: </label><span>{selectedId}</span>
-                    <br />
-                    <label>Color: </label>
-                    <input type='text' defaultValue={rectangles.find((r) => r.id === selectedId).fill} onChange={(e) => setNewColor(e.target.value)} />
-                    <button onClick={() => {
-                        const newRectangleIndex = rectangles.findIndex((r) => r.id === selectedId)
-                        const newRectangle = { ...rectangles[newRectangleIndex], fill: newColor }
-                        const newRectangles = rectangles
-                        newRectangles[newRectangleIndex] = newRectangle
-                        setRectangles(newRectangles)
-                        setSelectedId(null)
-                    } }>Change</button>
-                    <br />
-                    <span><i>* Supports hex codes without # and all named colors that <a href="https://htmlcolorcodes.com/color-names/">HTML supports</a>.</i></span>
-                </div>)}
-        </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setRectangles([])}>Clear</button>
+            <div className="w-full max-w-md">
+                <h1 className="text-3xl font-bold mb-4 mt-4">Selected Rectangle</h1>
+                {!selectedId
+                    ? (<span>None</span>)
+                    : (<div key={selectedId}>
+                        <div className="flex items-center justify-start mb-2">
+                        <div>
+                            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">Color: </label>
+                        </div>
+                        <div>
+                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type='text' defaultValue={rectangles.find((r) => r.id === selectedId).fill} onChange={(e) => setNewColor(e.target.value)} />
+                        </div>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2" onClick={() => {
+                            const newRectangleIndex = rectangles.findIndex((r) => r.id === selectedId)
+                            const newRectangle = { ...rectangles[newRectangleIndex], fill: newColor }
+                            const newRectangles = rectangles
+                            newRectangles[newRectangleIndex] = newRectangle
+                            setRectangles(newRectangles)
+                            setSelectedId(null)
+                        } }>Change</button>
+                        </div>
+                        <span><i>* Supports hex codes without # and all named colors that <a href="https://htmlcolorcodes.com/color-names/">HTML supports</a>.</i></span>
+                    </div>)}
+            </div>
         </>
     )
 }
